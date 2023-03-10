d71c695a65e9dfd052f8ccea2358a7de90577d36
Vincent Timothy Djaja <VincentTimothyDjaja@gmail.com>
2023-03-10 12:09:41 +0700

    update

--- Port_1.py
+++ Port_1.py
@@ -11,4 +11,30 @@ class childApp(GridLayout):
         self.cols = 2 
         self.add_widget(Label(text = 'Student Name'))
         self.s_name = TextInput()
-        
+        self.add_widget(self.s_name)
+
+        self.add_widget(Label(text = 'Student Marks'))
+        self.s_marks = TextInput()
+        self.add_widget(self.s_marks)
+
+        self.add_widget(Label(text = 'Student Gender'))
+        self.s_gender = TextInput()
+        self.add_widget(self.s_gender)
+
+        self.press = Button(text = 'Click Me')
+        self.press.bind(on_press = self.click_me)
+        self.add_widget(self.press)
+
+    def click_me(self, instance):
+        print("Name of students is " + self.s_name.text)
+        print("Marks of students is " + self.s_marks.text)
+        print("Gender of students is " + self.s_gender.text)
+        print("")
+
+class parentApp(App):
+    def build(self):
+        return childApp()
+
+if __name__ == "__main__":
+    parentApp().run()
+
